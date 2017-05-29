fc1bfcda329e535bf538521264a54794a24e37cb
georgyOstr <georgyostr@gmail.com>
2017-05-29 14:20:07 +0300

    Cleaned up my file.

--- GoshaStudy/goshafirsttask.cpp
+++ GoshaStudy/goshafirsttask.cpp
@@ -10,16 +10,21 @@ using namespace cv;
 
 int main(int argc, char *argv[])
 {
-  std::cout<<"Hello World! \n";
-  std::cout<<argc<<"\n";
   if (argc!=3)
     {
       std::cout<<"Add input, output file names.\n";
       return -1; //No need for ELSE
     } 
-  Mat image = imread("1.jpg");
+  Mat image = imread(argv[1]);
+  if (image.type()!=CV_8UC3)
+    {
+      std::cout<<"Not an image.";
+      return -1;
+    }
   putText(image, "This is the one special tree from FFX", Point2f(50,20), FONT_HERSHEY_PLAIN, 1, Scalar(0,0,250,0));
+  rectangle(image, Point2f(450,10),Point2f(500,20), Scalar(25,250,25,50));
   imshow("First one.", image);
   waitKey(0);
+  imwrite(argv[2], image);
   return 1;
 }
